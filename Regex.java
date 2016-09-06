66e4b9d5f7e26e6fa5c55b7966026c17f7f10c6a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-05 21:22:47 -0700

    explore regex with string.split method

--- Regex.java
+++ Regex.java
@@ -12,5 +12,11 @@ class Regex {
     } else {
       System.out.printf("%s is a NOT valid zip code\n", zipCode);
     }
+
+    String skills = c.readLine("Enter your job skills seperated by commas: ");
+    for(String s:skills.split("\\W+(and\\w+)?")) {
+      System.out.println("Great! We need someone with " + s + "!");
+    }
+
   }
 }
